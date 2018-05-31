d63742151181e5ad354c862f8b54f11585ea76f7
Triumphz <39701626+Triumphz@users.noreply.github.com>
2018-06-01 00:34:14 +0700

    Add files via upload

---
+++ TEST3/TEST2/TEST1/LOOP1.java
@@ -0,0 +1,16 @@
+package Triumphz;
+
+public class LOOP1
+{
+
+	public static void main(String[] args)
+	{
+		int i = 1;  /// ค่าเริ่มต้น
+				while (i<=100) // condition
+				{
+					System.out.println(i);
+					i=i+1;
+				}
+		
+}
+}
