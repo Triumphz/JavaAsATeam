c601f6ed45e70e82aa13119e5e5933b25b532617
BYRLaohanusorn <BYRLaohanusorn@hotmail.com>
2018-06-07 15:16:36 +0700

    Heng Indy Anvil

---
+++ Triumphz's Book/Heng Indy Anvil/Loop2/Loop2_1.java
@@ -0,0 +1,7 @@
+public class Loop2_1 {
+    public static void main(String[] args) {
+        System.out.println("===== ONE =====");
+        System.out.println("พิมพ์ XXXXXXXXXX");
+        System.out.print("XXXXXXXXXX");
+    }
+}
---
+++ Triumphz's Book/Heng Indy Anvil/Loop2/Loop2_2.java
@@ -0,0 +1,9 @@
+public class Loop2_2 {
+    public static void main(String[] args) {
+        System.out.println("===== TWO =====");
+        System.out.println("พิมพ์ XXXXXXXXXX 10 บรรทัด");
+        for(int i= 1; i<=10; i++){
+            System.out.println("XXXXXXXXXX");
+        }
+    }
+}
