56ebd94d7f91b6b6f19849b0636c38a33d7f4bbe
BYRLaohanusorn <BYRLaohanusorn@hotmail.com>
2018-06-07 15:42:44 +0700

    Heng Indy Anvil

---
+++ Triumphz's Book/Heng Indy Anvil/Loop2/Loop2_6.java
@@ -0,0 +1,17 @@
+public class Loop2_6 {
+    public static void main(String[] args) {
+        System.out.println("===== SIX =====");
+        System.out.format("พิมพ์ ----------X%n---------XXX%n--------XXXXX 10 บรรทัด%n");
+        int int_x = 1;
+        for(int max=10,i=max; i>=1; i--){
+            for(int l=1; l<=i; l++){
+                System.out.print("-");
+            }
+            for(int x=1; x<=int_x; x++){
+                System.out.print("X");
+            }
+            int_x+=2;
+            System.out.format("%n");
+        }
+    }
+}
