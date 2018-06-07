a8c6522933bd326fe3e83566b8c0cd37c65e4bf5
BYRLaohanusorn <BYRLaohanusorn@hotmail.com>
2018-06-07 14:53:10 +0700

    Heng Indy Anvil

---
+++ Triumphz's Book/Heng Indy Anvil/Loop1/Loop1_1.java
@@ -0,0 +1,8 @@
+public class Loop1_1 {
+    public static void main(String[] args) {
+        System.out.println("===== ONE =====");
+        for(int i= 1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
