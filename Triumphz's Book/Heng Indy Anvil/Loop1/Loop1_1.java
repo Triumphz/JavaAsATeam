6b40ce5816801883e4b0d1c0c53637dfae61a902
BYRLaohanusorn <BYRLaohanusorn@hotmail.com>
2018-06-07 14:56:41 +0700

    Heng Indy Anvil

--- Triumphz's Book/Heng Indy Anvil/Loop1/Loop1_1.java
+++ Triumphz's Book/Heng Indy Anvil/Loop1/Loop1_1.java
@@ -1,6 +1,7 @@
 public class Loop1_1 {
     public static void main(String[] args) {
         System.out.println("===== ONE =====");
+        System.out.println("พิมพ์ 1 ถึง 100");
         for(int i= 1; i<=100; i++){
             System.out.println(i);
         }
---
+++ Triumphz's Book/Heng Indy Anvil/Loop1/Loop1_2.java
@@ -0,0 +1,9 @@
+public class Loop1_2 {
+    public static void main(String[] args) {
+        System.out.println("===== TWO =====");
+        System.out.println("พิมพ์ 100 ถึง 10");
+        for(int i= 100; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
