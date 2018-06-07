051e12fe903faaaaa8efff32bd0f18bc0016a661
BYRLaohanusorn <BYRLaohanusorn@hotmail.com>
2018-06-07 15:02:21 +0700

    Heng Indy Anvil

---
+++ Triumphz's Book/Heng Indy Anvil/Loop1/Loop1_3.java
@@ -0,0 +1,11 @@
+public class Loop1_3 {
+    public static void main(String[] args) {
+        System.out.println("===== THREE =====");
+        System.out.println("พิมพ์ 1 ถึง 100 เฉพาะเลขคู่");
+        for(int i= 1; i<=100; i++){
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
