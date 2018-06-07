afe71665aca69660a1681fbe75acd506e895e513
BYRLaohanusorn <BYRLaohanusorn@hotmail.com>
2018-06-07 15:18:56 +0700

    Heng Indy Anvil

--- Triumphz's Book/Heng Indy Anvil/Loop2/Loop2_3.java
+++ Triumphz's Book/Heng Indy Anvil/Loop2/Loop2_3.java
@@ -1,9 +1,12 @@
-public class Loop2_2 {
+public class Loop2_3 {
     public static void main(String[] args) {
         System.out.println("===== TWO =====");
         System.out.println("พิมพ์ XXXXXXXXXX 10 บรรทัด");
         for(int i=1; i<=10; i++){
-            System.out.println("XXXXXXXXXX");
+            for(int l=1; l<=i; l++){
+                System.out.print("X");
+            }
+            System.out.format("%n");
         }
     }
 }
