277c60e81903b8266dbc36096d69d0e8d381f37a
BYRLaohanusorn <BYRLaohanusorn@hotmail.com>
2018-06-07 21:44:30 +0700

    Heng Indy ANvil

---
+++ Triumphz's Book/Heng Indy Anvil/Exercise1/Exercise1_5.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Exercise1_5 {
+    public static void main(String[] args) {
+        System.out.println("===== FIVE =====");
+        System.out.println("จงเขียนโปรแกรมรับข้อมูลเป็นองศาเซลเซียส (Celsius) เข้าทางคีย์บอร์ด แล้วทำการแปลงให้เป็นองศาฟาเรนไฮต์ (Fahrenheit) จากสูคร");
+        System.out.println("Celsius = (5 / 9) * (Fahrenheit - 32)");
+        System.out.println("Fahrenheit = (Celsius / (5 / 9)) + 32");
+        System.out.print("Enter Celsius : ");
+        float flt_celsius = sc.nextFloat();
+        System.out.format("Fahrenheit : (%.4f / (5 / 9)) + 32%n", flt_celsius);
+        System.out.format("Fahrenheit : %.4f", ((flt_celsius / (5.0 / 9)) + 32));
+    }
+}
