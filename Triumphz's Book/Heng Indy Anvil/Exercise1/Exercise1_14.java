2f88eff912c70bd116a3cc39b696d09c8db8030e
BYRLaohanusorn <BYRLaohanusorn@hotmail.com>
2018-06-08 11:05:57 +0700

    Heng Indy Anvil

--- Triumphz's Book/Heng Indy Anvil/Exercise1/Exercise1_14.java
+++ Triumphz's Book/Heng Indy Anvil/Exercise1/Exercise1_14.java
@@ -17,7 +17,10 @@ public class Exercise1_14 {
         arr_num[3] = sc.nextInt();
         System.out.print("จำนวนนักเรียนที่ได้ F : ");
         arr_num[4] = sc.nextInt();
-        System.out.format("AVG = 4 x %d + 3 x %d + 2 x %d + 1 x %d + 0 x %d%n", arr_num[0], arr_num[1], arr_num[2], arr_num[3], arr_num[4]);
-        System.out.format("AVG : %d,", (4*arr_num[0] + 3*arr_num[1] + 2*arr_num[2] + 1*arr_num[3] + 0*arr_num[4]));
+        int int_num = (4*arr_num[0] + 3*arr_num[1] + 2*arr_num[2] + 1*arr_num[3] + 0*arr_num[4]);
+        System.out.format("CAL = 4 x %d + 3 x %d + 2 x %d + 1 x %d + 0 x %d%n", arr_num[0], arr_num[1], arr_num[2], arr_num[3], arr_num[4]);
+        System.out.format("CAL : %d%n", int_num);
+        System.out.format("AVG : %d / %d%n", int_num, Arrays.stream(arr_num).sum());
+        System.out.format("AVG : %.4f%n", (((float)int_num) / Arrays.stream(arr_num).sum()));
     }
 }
